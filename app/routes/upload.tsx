8572520998bf30313ab4caac80716abb747e3e61
Thomas Testa <ttesta99@yahoo.com>
2025-08-12 14:37:30 -0400

    finish file upload

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,15 +1,33 @@
 'use client'
 import React, { useState, type FormEvent } from 'react'
+import FileUploader from '~/components/FileUploader';
 import Navbar from '~/components/Navbar'
 
 const upload = () => {
     const [isProcessing, setIsProcessing] = useState(false);
     const [statusText, setStatusText] = useState("");
+    const [file, setFile] = useState<File | null>(null);
+
+    const handleFileSelect = (file : File | null) => {
+        setFile(file);
+    }
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const form = e.currentTarget.closest('form');
 
+        if(!form) return;
+
+        const formData = new FormData(form);
+
+        const companyName = formData.get('company-name');
+        const jobTitle = formData.get('job-title');
+        const jobDescription = formData.get('job-description');
+
+        console.log({companyName, jobTitle, jobDescription, file})
     }
-  return (
+
+    return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover">
         <Navbar />
         <section className = "main-section">
@@ -42,7 +60,7 @@ const upload = () => {
 
                         <div className="form-div">
                             <label htmlFor="uploader">Upload Resume</label>
-                            <div>Uploader</div>
+                            <FileUploader onFileSelect={handleFileSelect}/>
                         </div>
 
                         <button className="primary-button" type='submit'>
@@ -53,7 +71,7 @@ const upload = () => {
             </div>
         </section>
     </main>
-  )
+    )
 }
 
 export default upload
