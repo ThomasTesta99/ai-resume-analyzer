4f97a958c88815247a665fe4979b9c11be86fc40
Thomas Testa <ttesta99@yahoo.com>
2025-08-13 12:39:00 -0400

    add ai to handle analyze

--- app/lib/puter.ts
+++ app/lib/puter.ts
@@ -350,7 +350,7 @@ export const usePuterStore = create<PuterStore>((set, get) => {
           ],
         },
       ],
-      { model: "claude-sonnet-4" }
+      { model: "claude-3-7-connet" }
     ) as Promise<AIResponse | undefined>;
   };
 
