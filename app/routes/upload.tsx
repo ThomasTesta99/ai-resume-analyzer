9ca73dddd34a82a4c22a8516fa0b2fd3f473c7af
Thomas Testa <ttesta99@yahoo.com>
2025-08-13 12:19:21 -0400

    starting pdf to image

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,15 +1,30 @@
 'use client'
 import React, { useState, type FormEvent } from 'react'
+import { useNavigate } from 'react-router';
 import FileUploader from '~/components/FileUploader';
 import Navbar from '~/components/Navbar'
+import { usePuterStore } from '~/lib/puter';
 
 const upload = () => {
+    const {auth, isLoading, fs, ai, kv} = usePuterStore(); // fs = file storage, kv = key value storage functions
+    const navigate = useNavigate();
     const [isProcessing, setIsProcessing] = useState(false);
     const [statusText, setStatusText] = useState("");
     const [file, setFile] = useState<File | null>(null);
 
     const handleFileSelect = (file : File | null) => {
         setFile(file);
+    }
+
+    const handleAnalyze = async ({companyName, jobTitle, jobDescription, file}: {companyName: string, jobTitle: string, jobDescription: string, file: File}) => {
+        setIsProcessing(true);
+        setStatusText("Uploading the file...");
+        const uploadedFile = await fs.upload([file]); 
+
+        if(!uploadedFile) return setStatusText("Error: Failed to upload file");
+
+        setStatusText("Converting to image...");
+        //const imageFile = await convertPdfToIUmage(file);
     }
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
@@ -20,57 +35,58 @@ const upload = () => {
 
         const formData = new FormData(form);
 
-        const companyName = formData.get('company-name');
-        const jobTitle = formData.get('job-title');
-        const jobDescription = formData.get('job-description');
+        const companyName = formData.get('company-name') as string;
+        const jobTitle = formData.get('job-title') as string;
+        const jobDescription = formData.get('job-description') as string;
 
-        console.log({companyName, jobTitle, jobDescription, file})
+        if(!file) return;
+        handleAnalyze({companyName, jobTitle, jobDescription, file});
     }
 
     return (
-    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
-        <Navbar />
-        <section className = "main-section">
-            <div className="page-heading py-16">
-                <h1>Smart feedback for your dream job</h1>
-                {isProcessing ? (
-                    <>
-                        <h2>{statusText}</h2>
-                        <img src="/images/resume-scan.gif" className='w-full' />
-                    </>
-                ): (
-                    <h2>Drop your resume for an ATS score and improvement tips</h2>
-                )}
-                {!isProcessing && (
-                    <form id='upload-form' onSubmit={handleSubmit} className='flex flex-col gap-4 mt-8'>
-                        <div className="form-div">
-                            <label htmlFor="company-name">Company Name</label>
-                            <input type="text" name='company-name' placeholder='Company Name' id='company-name'/>
-                        </div>
+        <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+            <Navbar />
+            <section className = "main-section">
+                <div className="page-heading py-16">
+                    <h1>Smart feedback for your dream job</h1>
+                    {isProcessing ? (
+                        <>
+                            <h2>{statusText}</h2>
+                            <img src="/images/resume-scan.gif" className='w-full' />
+                        </>
+                    ): (
+                        <h2>Drop your resume for an ATS score and improvement tips</h2>
+                    )}
+                    {!isProcessing && (
+                        <form id='upload-form' onSubmit={handleSubmit} className='flex flex-col gap-4 mt-8'>
+                            <div className="form-div">
+                                <label htmlFor="company-name">Company Name</label>
+                                <input type="text" name='company-name' placeholder='Company Name' id='company-name'/>
+                            </div>
 
-                        <div className="form-div">
-                            <label htmlFor="job-title">Job Title</label>
-                            <input type="text" name='job-title' placeholder='Job Title' id='job-title'/>
-                        </div>
+                            <div className="form-div">
+                                <label htmlFor="job-title">Job Title</label>
+                                <input type="text" name='job-title' placeholder='Job Title' id='job-title'/>
+                            </div>
 
-                        <div className="form-div">
-                            <label htmlFor="job-description">Job Description</label>
-                            <textarea rows={5} name='job-description' placeholder='Job Description' id='job-description'/>
-                        </div>
+                            <div className="form-div">
+                                <label htmlFor="job-description">Job Description</label>
+                                <textarea rows={5} name='job-description' placeholder='Job Description' id='job-description'/>
+                            </div>
 
-                        <div className="form-div">
-                            <label htmlFor="uploader">Upload Resume</label>
-                            <FileUploader onFileSelect={handleFileSelect}/>
-                        </div>
+                            <div className="form-div">
+                                <label htmlFor="uploader">Upload Resume</label>
+                                <FileUploader onFileSelect={handleFileSelect}/>
+                            </div>
 
-                        <button className="primary-button" type='submit'>
-                            Analyze Resume
-                        </button>
-                    </form>
-                )}
-            </div>
-        </section>
-    </main>
+                            <button className="primary-button" type='submit'>
+                                Analyze Resume
+                            </button>
+                        </form>
+                    )}
+                </div>
+            </section>
+        </main>
     )
 }
 
