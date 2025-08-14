3c81b992c55f30bfc6038adc8121fe1ab1ac4d9c
Thomas Testa <ttesta99@yahoo.com>
2025-08-14 09:36:11 -0400

    make components

---
+++ app/components/ATS.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ATS = () => {
+  return (
+    <div>
+      ATS
+ATS
+    </div>
+  )
+}
+
+export default ATS
---
+++ app/components/Summary.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Summary = () => {
+  return (
+    <div>
+      Summary
+Summary
+    </div>
+  )
+}
+
+export default Summary
